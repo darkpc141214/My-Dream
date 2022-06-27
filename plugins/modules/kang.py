418b82e53773589ee4575256a3359bc4de01b21c
darkpc141214 <104191922+darkpc141214@users.noreply.github.com>
2022-06-27 19:30:15 +0530

    Update kang.py

--- plugins/modules/kang.py
+++ plugins/modules/kang.py
@@ -22,7 +22,7 @@ from telegram import Message, Chat, MessageEntity, InlineQueryResultArticle
 from os import path
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger()
+Logger = logging.getLogger()
 
 def getConfig(name: str):
     return os.environ[name]
